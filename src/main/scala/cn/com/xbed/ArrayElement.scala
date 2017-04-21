a33e9d34d6b48abb5077b851b3af15b203ee4c3a
lit <lit@xbed.com.cn>
2017-04-21 19:07:10 +0800

    test

--- src/main/scala/cn/com/xbed/ArrayElement.scala
+++ src/main/scala/cn/com/xbed/ArrayElement.scala
@@ -3,7 +3,12 @@ package cn.com.xbed
 /**
   * Created by root on 17-4-21.
   */
-class ArrayElement(conts: Array[String]) extends Element{
-  override def contents: Array[String] = conts
-  val contents1: Array[String] = conts
-}
+//class ArrayElement(conts: Array[String]) extends Element{
+//  override def contents: Array[String] = conts
+//  val contents1: Array[String] = conts
+//}
+
+//class ArrayElement(val contents :Array[String]) extends Element {}
+//class ArrayElement(val x :Array[String]) extends Element{val contents: Array[String] = x}
+//class Cat {val a = false}
+//class Tiger (override val a : Boolean, private var b : Int){}
