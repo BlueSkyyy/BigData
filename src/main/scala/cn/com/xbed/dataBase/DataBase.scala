ad7ed3084a3289bf58f00967ef324e092864bec3
lit <lit@xbed.com.cn>
2017-04-26 10:25:26 +0800

    add comment

--- src/main/scala/cn/com/xbed/dataBase/DataBase.scala
+++ src/main/scala/cn/com/xbed/dataBase/DataBase.scala
@@ -7,7 +7,7 @@ import java.sql.{Connection, DriverManager}
   */
 object DataBase {
   def main(args: Array[String]): Unit = {
-    // connect to the database named "mysql" on the localhost
+    // 配置数据库连接参数
     val driver = "com.mysql.jdbc.Driver"
     val url = "jdbc:mysql://localhost/xbed_business"
     val username = "root"
@@ -16,21 +16,25 @@ object DataBase {
     var connection:Connection = null
 
     try {
-
+      // 加载驱动类
       Class.forName(driver)
+      // 创建数据库链接
       connection = DriverManager.getConnection(url, username, password)
-
-      val statement = connection.createStatement()
-      val resultSet = statement.executeQuery("select * from orders where user_id = 1")
+      // 执行语句
+      val resultSet = connection.createStatement().executeQuery("select * from orders where user_id = 1")
+      // 处理查询结果
       while ( resultSet.next() ) {
         val name = resultSet.getString("name")
         val phone = resultSet.getString("phone")
         val orderNo = resultSet.getString("order_no")
-        println("name, phone, orderNo = " + name + ", " + phone + ", " + orderNo)
+        val createTime = resultSet.getString("created_at")
+        println(String.format("姓名:%s, 手机:%s, 订单号:%s, 创建时间:%s", name, phone, orderNo, createTime));
       }
     } catch {
       case e => e.printStackTrace
+    } finally {
+      // 关闭数据库连接
+      connection.close()
     }
-    connection.close()
   }
 }
